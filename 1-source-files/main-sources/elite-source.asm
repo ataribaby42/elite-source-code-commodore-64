5c4b6bb05f31016cbea145f7269c1aef351a016e
Mark Moxon <mark@moxon.net>
2025-02-19 11:41:46 +0000

    Mention that DKSANYKEY is unused

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -34627,7 +34627,8 @@ ENDIF
 ;       Name: DKSANYKEY
 ;       Type: Subroutine
 ;   Category: Keyboard
-;    Summary: Scan a specific column in the keyboard matrix for a key press
+;    Summary: An unused routine that scans a specific column in the keyboard
+;             matrix for a key press
 ;
 ; ------------------------------------------------------------------------------
 ;
