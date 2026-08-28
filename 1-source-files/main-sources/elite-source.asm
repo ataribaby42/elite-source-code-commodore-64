392ce7f251bdbec0b7435645d6b58642f4e27ab0
ataribaby <ataribaby@seznam.cz>
2026-08-28 19:52:40 +0200

    Energy Bomb/IFF Unit price fixes

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -24283,11 +24283,11 @@ ELSE                   ; ELITE: Unbound build option (else)
  
 IF _IFF_UNIT            ; ATARIBABY I.F.F. unit replaces Energy Bomb
   
- EQUW 25000             ; 8  I.F.F. Unit               1500.0 Cr
+ EQUW 25000             ; 8  I.F.F. Unit               2500.0 Cr
  
 ELSE 
  
- EQUW 900               ; 8  Energy Bomb                900.0 Cr
+ EQUW 9000              ; 8  Energy Bomb                900.0 Cr
   
 ENDIF
  
